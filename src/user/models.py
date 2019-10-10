8e384f9bf3f1e87196127b84e6906f5cbb3a21aa
mike Tetteh <miketetteh751@outlook.com>
2019-10-10 18:04:35 +0000

    messages models created

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from mailinglist.models import MailingList
+
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          editable=False),
+    mailing_list = models.ForeignKey(to=MailingList, on_delete=models.CASCADE),
+    subject = models.CharField(max_length=140),
+    body = models.TextField()
+    started = models.DateTimeField(default=None, null=True),
+    finished = models.DateTimeField(default=None, null=True)
