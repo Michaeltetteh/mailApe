622359dfba1a953b4172e7604957e843b3056537
mike Tetteh <miketetteh751@outlook.com>
2019-10-11 21:02:03 +0000

    creating mailinglist views

--- src/config/settings.py
+++ src/config/settings.py
@@ -117,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# to config crispy forms to use bootstrap4
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- src/mailinglist/urls.py
+++ src/mailinglist/urls.py
@@ -5,5 +5,7 @@ from mailinglist import views
 app_name = 'mailinglist'
 
 urlpatterns = [
-
+    path('',
+         views.MailingListView.as_view(),
+         name='mailinglist_list'),
 ]
